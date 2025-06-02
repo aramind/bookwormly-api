df6f9378ff9bd230127cee63beb4d6c09b76f30c
aramind <88814622+aramind@users.noreply.github.com>
2025-06-03 00:55:08 +0800

    feat: adding the route to handle deleting

--- src/controllers/book/bookController.js
+++ src/controllers/book/bookController.js
@@ -1,9 +1,11 @@
 const addReview = require("./addReview");
 const getBooks = require("./getBooks");
+const deleteBook = require("./deleteBook");
 
 const bookController = {
   addReview,
   getBooks,
+  deleteBook,
 };
 
 module.exports = bookController;
