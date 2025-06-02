fd5242acef5dace556c82af251f697c93becc689
aramind <88814622+aramind@users.noreply.github.com>
2025-06-02 23:32:48 +0800

    feat: adding book schema

---
+++ src/models/Book.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const BookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", BookSchema);
