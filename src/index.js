2b3effc87289ae1708fc7ebc53c163895c3a9b25
aramind <88814622+aramind@users.noreply.github.com>
2025-06-02 19:57:22 +0800

    setup: initial set up with mongodb db live

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+
+// env
+dotenv.config();
+const { PORT = 3001, MONGO_CONNECT: DB } = process.env;
+
+// middlewares
+app.use([helmet(), express.json()]);
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+
+// if not found
+app.use((req, res) =>
+  res.status(404).json({ success: false, message: "Not found" })
+);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB);
+    app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
