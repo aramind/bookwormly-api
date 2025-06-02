3e579e6329e69d87e3406b7f29fc167f32232f49
aramind <88814622+aramind@users.noreply.github.com>
2025-06-02 21:40:24 +0800

    feat: adding the user model

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  profileImage: {
+    type: String,
+    default: "",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
